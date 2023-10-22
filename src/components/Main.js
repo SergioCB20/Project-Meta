e943d27939485d509ee4d26f40a6387aabab8189
Sergio <sergiochumbi98@gmail.com>
2023-10-22 02:34:06 -0500

    sixth commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
+import HeroImg from "../assets/icons_assets/restauranfood.jpg"
 import '../App.css';
 
 function Hero(){
     return(
         <div className='hero'>
             <h1>Little Lemon</h1>
+            <img src={HeroImg} alt='Restaurant Food - Hero Image'/>
             <h2>Chicago</h2>
             <p>We are a family owned Mediterrean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <button>Reserve a table</button>
+        </div>
+    )
+}
+
+function Specials(){
+    return(
+        <div className='specials'>
+            <div className='specials-header'>
+                <h1>Specials</h1>
+            </div>
         </div>
     )
 }
@@ -17,6 +30,7 @@ function Main(){
         <main>
             <Hero/>
             <div className='bg-hero'/>
+            <Specials/>
         </main>
     )
 }
